b607add585e36d67e3e863ccfeb6eae557181ada
Chicodev30 <chicodev30@gmail.com>
2025-01-30 17:04:37 -0300

    adicionado 10665 plugin

--- src/project_name/settings.py
+++ src/project_name/settings.py
@@ -85,6 +85,15 @@ loaders = TEMPLATES[0]["OPTIONS"].get("loaders") or [
 TEMPLATES[0]["OPTIONS"]["loaders"] = loaders
 TEMPLATES[0].pop("APP_DIRS", None)
 
+MAPSTORE_PROJECTION_DEFS = [
+{
+    "code": "EPSG:10665",
+    "def": "+proj=tmerc +lat_0=0 +lon_0=-51 +k=0.999995 +x_0=300000 +y_0=5000000 +ellps=GRS80 +units=m +no_defs",
+    "extent": [-20037508.34, -20048966.1, 20037508.34, 20048966.1],
+    "worldExtent": [-180.0, -85.06, 180.0, 85.06]
+}
+]
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": True,
